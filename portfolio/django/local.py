e8750699b17eed6940a87c371547560bfd0f6446
Mian sadaf <miansadafali@gmail.com>
2025-02-15 17:21:16 +0500

    fixed bugs

--- portfolio/django/local.py
+++ portfolio/django/local.py
@@ -1,7 +1,5 @@
 from .base import *
 
-DEBUG = True
-ALLOWED_HOSTS = ['*']
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
--- portfolio/django/production.py
+++ portfolio/django/production.py
@@ -34,4 +34,3 @@ STATICFILES_DIRS = [
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
 
-django_heroku.settings(locals()) 
