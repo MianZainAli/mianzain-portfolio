d3f60ab2e9c93cf5edd90ad288a5ff227b4af91f
Mian sadaf <miansadafali@gmail.com>
2025-02-15 18:31:25 +0500

    static file error fix

--- portfolio/django/production.py
+++ portfolio/django/production.py
@@ -30,7 +30,6 @@ AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
