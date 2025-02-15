6113b97066ef8c58020c2da182a4a604dcda2fd3
Mian sadaf <miansadafali@gmail.com>
2025-02-15 17:29:55 +0500

    fixed bugs

--- portfolio/django/base.py
+++ portfolio/django/base.py
@@ -128,3 +128,11 @@ CKEDITOR_CONFIGS = {
         ]
     }
 }
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
