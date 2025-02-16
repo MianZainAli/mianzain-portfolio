d95d6cc051e9f88417718ad23355e2712cca861f
Mian sadaf <miansadafali@gmail.com>
2025-02-16 15:59:41 +0500

    fixed AWS S3 credentials

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -144,7 +144,7 @@ CKEDITOR_CONFIGS = {
     }
 }
 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATICFILES_DIRS = [
